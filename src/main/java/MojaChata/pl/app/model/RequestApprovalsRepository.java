1cffa890667c6cd3045e6cc3f378125be0f60f87
t <tomekk925@gmail.com>
2025-01-07 06:08:35 +0100

    removed unnecessary code, added review deletion

--- src/main/java/MojaChata/pl/app/model/RequestApprovalsRepository.java
+++ src/main/java/MojaChata/pl/app/model/RequestApprovalsRepository.java
@@ -3,7 +3,6 @@ package MojaChata.pl.app.model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface RequestApprovalsRepository extends CrudRepository<RequestApproval, Long> {
--- src/main/java/MojaChata/pl/app/model/RequestService.java
+++ src/main/java/MojaChata/pl/app/model/RequestService.java
@@ -5,11 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class RequestService {
